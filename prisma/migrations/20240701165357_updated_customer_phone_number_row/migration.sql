f6ab44e7c5f04fe07da303947fd91742b5a1340c
Michael Akingbade <akingbademichael7@gmail.com>
2024-07-19 17:27:42 +0100

    deleted migrations

--- prisma/migrations/20240701165357_updated_customer_phone_number_row/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `phoneNumber` on the `customers` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "customers" DROP COLUMN "phoneNumber",
-ADD COLUMN     "phoneNumber" INTEGER NOT NULL;
--- prisma/migrations/20240701194828_phone_number_field_update/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "customers" ALTER COLUMN "phoneNumber" SET DATA TYPE VARCHAR(15);
